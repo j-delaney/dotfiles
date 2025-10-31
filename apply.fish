ed974b3468a773c0675d98981e14da5d8ca057e5
J Delaney <jdelaney@stripe.com>
2025-10-31 15:45:13 -0700

    Initial apply script

---
+++ apply.fish
@@ -0,0 +1,33 @@
+#!/usr/bin/env fish
+
+# Greatly inspired by https://github.com/dbalatero/dotfiles/blob/main/apply
+
+set dotfiles_dir (realpath (dirname (status --current-filename)))
+
+function symlink -a src destination
+    set --function full_src_path "$dotfiles_dir/$src"
+
+    if not test -e "$destination"
+        echo "Symlinking $full_src_path -> $destination"
+        mkdir -p (dirname "$destination")
+        ln -s "$full_file_path" "$destination"
+    else 
+        echo "[WARN] Skipping symlink $full_src_path -> $destination due to already existing"
+    end
+end
+
+function force_symlink -a src destination
+    set --function full_src_path "$dotfiles_dir/$src"
+    echo "Symlinking $full_src_path -> $destination with overwrite"
+    mkdir -p (dirname "$destination")
+    ln -s "$full_file_path" "$destination"
+end
+
+# Fish
+symlink fish/config.fish "$HOME/.config/fish/config.fish"
+for f in fish/functions/*
+    symlink $f "$HOME/.config/fish/functions/"(basename $f)
+end
+for f in fish/conf.d/*
+    symlink $f "$HOME/.config/fish/conf.d/"(basename $f)
+end
