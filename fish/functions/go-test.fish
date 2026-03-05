4a5cda626be2d858bcc3312f2bcbf136d69a8d22
J Delaney <jdelaney@stripe.com>
2026-03-04 18:00:56 -0800

    Fish: go-test actually use depth provided

--- fish/functions/go-test.fish
+++ fish/functions/go-test.fish
@@ -48,7 +48,9 @@ function go-test
         for target in $targets
             set -f rdeps_file "$JDOME_PATH/$module/$target.rdeps"
             if test -e "$rdeps_file"
-                set -f rdep_targets (awk '$1 > 1 { next } {print $2}' $rdeps_file | sed "s|^$module|.|")
+                set -f rdep_targets (awk '$1 > '$jdome_depth' { next } {print $2}' $rdeps_file | sed "s|^$module|.|")
+                echo $rdep_targets
+                return 0
                 echo "Running "(count $rdep_targets)"indirect tests for $target"
                 go test $rdep_targets
             else
