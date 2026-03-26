63cd8ff5ec7bfc469fd333b1debe2a6f6bb16b88
J Delaney <jdelaney@stripe.com>
2026-03-26 13:50:37 -0700

    Fish: refresh-prompt-on-cmd

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -29,3 +29,7 @@ function fish_prompt --description 'Write out the prompt'
     
         echo -n -s (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
 end
+
+function fish_right_prompt
+    rpoc_time
+end
